88bb419799b47e5d9a9dd4afdefea6e9577ba03f
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2024-04-13 02:48:56 +0300

    Swap parameters $name and $class in types::alias()

--- tests/TypeStringifier/TypeStringifierTest.php
+++ tests/TypeStringifier/TypeStringifierTest.php
@@ -130,7 +130,7 @@ final class TypeStringifierTest extends TestCase
         yield [types::conditional(types::template('T', types::atFunction('trim')), if: types::string, then: types::int, else: types::float), '(T@trim() is string ? int : float)'];
         yield [types::array(value: types::varianceAware(types::int, Variance::Covariant)), 'array<covariant int>'];
         yield [types::offset(types::template('T', types::atClass('A')), types::literalValue('abc')), "T@A['abc']"];
-        yield [types::alias('A', 'Some'), 'Some@A'];
+        yield [types::alias('Some', 'A'), 'Some@A'];
         yield [types::static('X\\Y'), 'static@X\\Y'];
         yield [types::static('X\\Y', types::string), 'static@X\\Y<string>'];
     }
