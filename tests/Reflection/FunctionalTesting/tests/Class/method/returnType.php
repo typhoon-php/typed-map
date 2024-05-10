c257a2c6d36f61bc5ace96197962731a7f9ce20e
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2024-05-10 06:56:47 +0300

    Reintroduce PhpDocParser

---
+++ tests/Reflection/FunctionalTesting/tests/Class/method/returnType.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Typhoon\Reflection\FunctionalTesting;
+
+use Typhoon\Reflection\Kind;
+use Typhoon\Reflection\TyphoonReflector;
+use Typhoon\Type\types;
+use function PHPUnit\Framework\assertEquals;
+use function Typhoon\DeclarationId\classId;
+
+return static function (TyphoonReflector $reflector): void {
+    $reflection = $reflector->reflectCode(
+        <<<'PHP'
+            <?php
+            
+            interface A
+            {
+                /**
+                 * @return non-empty-string
+                 */
+                public function a(): string;
+            }
+            PHP,
+    )[classId('A')]->method('a') ?? throw new \LogicException();
+
+    assertEquals(types::string, $reflection->returnType(Kind::Native));
+    assertEquals(types::nonEmptyString, $reflection->returnType(Kind::Annotated));
+    assertEquals(types::nonEmptyString, $reflection->returnType(Kind::Resolved));
+};
