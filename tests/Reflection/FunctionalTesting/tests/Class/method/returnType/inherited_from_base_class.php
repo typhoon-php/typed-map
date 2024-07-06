0e78035a5a94f0dba4588335207b8d5a04afff86
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2024-07-06 23:10:47 +0300

    Rename types::int() -> intRange(), *Value() -> *()

--- tests/Reflection/FunctionalTesting/tests/Class/method/returnType/inherited_from_base_class.php
+++ tests/Reflection/FunctionalTesting/tests/Class/method/returnType/inherited_from_base_class.php
@@ -30,6 +30,6 @@ return static function (TyphoonReflector $reflector): void {
     )[namedClassId('B')]->methods['a'];
 
     assertEquals(types::string, $reflection->returnType(Kind::Native));
-    assertEquals(types::stringValue('a'), $reflection->returnType(Kind::Annotated));
-    assertEquals(types::stringValue('a'), $reflection->returnType(Kind::Resolved));
+    assertEquals(types::string('a'), $reflection->returnType(Kind::Annotated));
+    assertEquals(types::string('a'), $reflection->returnType(Kind::Resolved));
 };
