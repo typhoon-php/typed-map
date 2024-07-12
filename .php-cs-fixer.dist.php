2ef8866cb975851e76cade39b7dd0f6e5c172a60
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2024-07-13 01:09:37 +0300

    Remove empty line between constants if no phpDoc

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -29,6 +29,13 @@ $config = (new Config())
     'strict_comparison' => false,
     'logical_operators' => false,
     'ordered_class_elements' => ['order' => ['use_trait']],
+    'class_attributes_separation' => ['elements' => [
+        'trait_import' => 'only_if_meta',
+        'const' => 'only_if_meta',
+        'case' => 'only_if_meta',
+        'property' => 'one',
+        'method' => 'one',
+    ]],
 ]);
 
 return $config;
