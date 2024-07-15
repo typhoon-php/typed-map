da392ef7143d7c020bbe082300b19392b685050a
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2024-07-16 02:17:34 +0300

    Improve multiple anonymous classes on line handling, add tests

---
+++ tests/Reflection/FunctionalTesting/tests/class/multiple_anonymous_classes_on_line.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Typhoon\Reflection;
+
+use PHPUnit\Framework\TestCase;
+use Typhoon\ChangeDetector\InMemoryChangeDetector;
+use Typhoon\Reflection\Internal\Data\Data;
+use Typhoon\Reflection\Internal\TypedMap\TypedMap;
+
+return static function (TyphoonReflector $reflector, TestCase $test): void {
+    $reflector = $reflector->withResource(new Resource(
+        '<?php new class {}; new class {};',
+        (new TypedMap())
+            ->set(Data::File, 'some.php')
+            ->set(Data::UnresolvedChangeDetectors, [new InMemoryChangeDetector()]),
+    ));
+
+    $test->expectExceptionMessage('because 2 anonymous classes are declared at columns 11, 25');
+
+    $reflector->reflectAnonymousClass('some.php', 1);
+};
